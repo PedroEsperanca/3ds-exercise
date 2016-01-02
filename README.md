46d17cc59c1778e585030071ed53040b1ea8655e
pedroesperanca <buesimples@gmail.com>
2016-01-02 19:12:23 +0000

    tests

---
+++ README.md
@@ -0,0 +1,13 @@
+# D3JS-radials-with-graphics-inside
+
+## Live URL
+<http://pedroesperanca.github.io/D3JS-radials-with-graphics-inside/>
+
+## Testing
+run
+````jasmine````
+from the command line
+
+## Improvements needed
+* use a CSS extension language for scalability
+* integrate the html fully into D3JS
--- scripts/models.js
+++ scripts/models.js
@@ -1,21 +1,31 @@
-define('models', function (require) {
+var models = {
+	revenue: {
+		total: 200000,
+		tablet: 120000,
+		smartphone: 80000
+	},
+	impressions: {
+		total: 50000000,
+		tablet: 20000000,
+		smartphone: 30000000
+	},
+	visits: {
+		total: 600000000,
+		tablet: 480000000,
+		smartphone: 120000000
+	},
+	percentage: function(field, total){
+		return (field/total)*100
+	},
+};
 
-	return models = {
-		revenue: {
-			total: 200000,
-			tablet: 120000,
-			smartphone: 80000
-		},
-		impressions: {
-			total: 50000000,
-			tablet: 20000000,
-			smartphone: 30000000
-		},
-		visits: {
-			total: 600000000,
-			tablet: 480000000,
-			smartphone: 120000000
-		}
-	};
-
-})
+//export for requirejs
+if(typeof module === 'undefined') {
+	define('models', function (require) {
+		return models;
+	});
+} 
+//export for nodejs
+else if(typeof module !== 'undefined') {
+	module.exports = models;
+}
